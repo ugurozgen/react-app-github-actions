59c8a4ce06d96595c24c00df50c5d43687ffdce9
ugurozgen <ugurozgenn@gmail.com>
2020-09-03 23:39:39 +0300

    fix: fix

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,14 +2,25 @@ const core = require("@actions/core")
 const github = require("@actions/github")
 
 try {
-  throw (new Error("some error message"))
-  const name = core.getInput("who-to-greet")
-  console.log(`hello ${name}`)
+    //   throw new Error("some error message")
 
-  const time = new Date()
-  core.setOutput("time", time.toTimeString())
+    core.debug("debug message") // if debug enabled from secret ACTIONS_STEP_DEBUG=true
+    core.warning("warning message") // yellow logs
+    core.error("error message") // red logs
 
-  console.log(JSON.stringify(github, null, "\t"))
+    const name = core.getInput("who-to-greet")
+
+    core.setSecret(name) // it will be **** in logs
+    console.log(`hello ${name}`)
+
+    const time = new Date()
+    core.setOutput("time", time.toTimeString())
+
+    core.startGroup("Logging github object") // it will start expandable logs
+    console.log(JSON.stringify(github, null, "\t"))
+    core.endGroup
+
+    core.exportVariable("HELLO", "hello") // we can use this env variable in another step in action, like actions/workflows/actions.yml:14
 } catch (error) {
-  core.setFailed("failed")
+    core.setFailed("failed")
 }
--- release.config.js
+++ release.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  branches: "master",
-  repositoryUrl: "https://github.com/ugurozgen/react-app-github-actions",
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        assets: [
-          { path: "build.zip", label: "Build" },
-          { path: "coverage.zip", label: "coverage" },
+    branches: "master",
+    repositoryUrl: "https://github.com/ugurozgen/react-app-github-actions",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/github",
+            {
+                assets: [
+                    { path: "build.zip", label: "Build" },
+                    { path: "coverage.zip", label: "coverage" },
+                ],
+            },
         ],
-      },
     ],
-  ],
 }
